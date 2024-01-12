43b2140e14002074e34f0e5bbbd3989134c2f422
sree7k7 <sree7k7@gmail.com>
2024-01-12 12:04:26 +0100

    added get and post for multi lambda fn

--- api_lambda/api_lambda_stack.py
+++ api_lambda/api_lambda_stack.py
@@ -1,19 +1,74 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+from aws_cdk import aws_lambda as lambda_
+from aws_cdk import aws_apigateway as apigw
+from aws_cdk import aws_iam as iam
 
+from aws_cdk import (
+    Stack,
+)
 class ApiLambdaStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # create a lambda function hello world 
+        lambda_function = lambda_.Function(
+            self, "HelloHandler",
+            runtime=lambda_.Runtime.PYTHON_3_10,
+            code=lambda_.Code.from_asset('lambda'),
+            handler='helloworld.lambda_handler', # file name is helloworld.py, function name is lambda_handler
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ApiLambdaQueue",
-        #     visibility_timeout=Duration.seconds(300),
+        # create a lambda function create s3 bucket
+        lambda_function_2 = lambda_.Function(
+            self, "HelloPostHandler",
+            runtime=lambda_.Runtime.PYTHON_3_10,
+            code=lambda_.Code.from_asset('lambda'),
+            handler='post_helloworld.lambda_handler', # file name is create_s3.py, function name is lambda_handler
+        )
+
+        # create an api gateway with a lambda integration
+        api = apigw.LambdaRestApi(
+            self, 'Endpointsz',
+            handler=lambda_function,
+            deploy=True,    
+            deploy_options=apigw.StageOptions(
+                stage_name='dev',
+            ),
+        )
+
+        # add a GET, post method to the root resource of the API
+        api.root.add_method('GET', apigw.LambdaIntegration(lambda_function))
+        api.root.add_method('POST', apigw.LambdaIntegration(lambda_function_2))
+
+        # define a deployment
+        # dev_deployment = apigw.Deployment(
+        #     self, 'Deployment',
+        #     api=api,
+        #     description='deployment for dev and prod',
+        # )
+
+        # prod_deployment = apigw.Deployment(
+        #     self, 'prod_Deployment',
+        #     api=prod.api,
+        #     description='deployment for dev and prod',
+        # )
+
+        # add dev stage to api gateway
+        # dev_stage = apigw.Stage(
+        #     self, 'dev',
+        #     deployment=dev_deployment,
+        #     stage_name='dev',
+        # )
+
+        # add prod stage to api gateway
+        # prod_stage = apigw.Stage(
+        #     self, 'prod',
+        #     deployment=prod_deployment,
+        #     stage_name='prod',
         # )
---
+++ lambda/post_helloworld.py
@@ -0,0 +1,11 @@
+# create a hello world lambda function
+
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    # this is comment
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from --post-- Lambda!')
+    }
